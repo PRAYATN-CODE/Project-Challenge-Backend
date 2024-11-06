bd19be845973862d228e0f063f58914ff792b563
PRAYATN-CODE <prayatansoni166@gmail.com>
2024-11-06 17:18:39 +0530

    cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || 5000;
 
 // Enable CORS for localhost:3000 (development) and your Render URL (production)
 const corsOptions = {
-    origin: ['https://notes-app-backend-zq23.onrender.com'],  // Add both development and production URLs
+    origin: ['https://notes-app-backend-nwvi.onrender.com'],  // Add both development and production URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,  // Allow cookies if needed
