ce7c5efc99d968189fdb6710c711523c4274a94a
PRAYATN-CODE <prayatansoni166@gmail.com>
2025-01-11 16:59:29 +0530

    database online

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 // const mongoURL = "mongodb+srv://PrayatnSoni:7SSwBwCGv6ltTkAo@prayatn.ulop1.mongodb.net/?retryWrites=true&w=majority&appName=Prayatn";
-const mongoURL = "mongodb://localhost:27017/notebook";
+const mongoURL = "mongodb+srv://prayatnsoni-aichatconnect:BIw8Sf6zAFrScpHN@prayatn-ai-chatconnect.rusvl.mongodb.net/project-challenge";
 // const mongoURL = "mongodb+srv://PrayatnSoni:7SSwBwCGv6ltTkAo@prayatn.ulop1.mongodb.net/?retryWrites=true&w=majority&appName=Prayatn";
 
 const connectToMongo = async () => {
